dba05eeb51453af668c0274b89a117f254a68613
MoocoW97 <h.a.stinson19@gmail.com>
2024-06-24 08:25:13 -0700

    Add files via upload

---
+++ scripts/main.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var max_enemies : int
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	max_enemies = 20
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+
+func _on_enemy_spawner_hit_p():
+	pass
